14a22984f80322072a3fdad157c89f286a353403
k <kate.a.mccurdy@gmail.com>
2014-01-29 22:51:05 +0100

    stepper motor works now !yay!

--- server/server.py
+++ server/server.py
@@ -29,36 +29,45 @@ sock.bind(("0.0.0.0", port))
 sock.setblocking(0)
  
 # connect to arduino  
-"""
+
 locations=['/dev/tty.usbmodem411', '/dev/tty.usbmodem641', '/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3', 
 '/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3']    
     
 for device in locations:    
     try:    
         print "Trying...",device  
-        #arduino = serial.Serial(device, 9600)   
+        arduino = serial.Serial(device, 9600)   
+        print 'Located device!'
+        time.sleep(1)
+        print arduino.readline()
+        print arduino.readline()
         break  
     except:    
         print "Failed to connect on",device     
-"""
+
   
 # ============
 # main program
 # ============
 
 running = 1
+
 while running:
 
     result = select.select([sock], [], [], 0)
     if len(result[0]) > 0:
         # read in data
-        data = result[0][0].recvfrom(1024)
-        print "Recieved:", data
+        data = str(result[0][0].recvfrom(1024)[0]) + '\n'
+        print "Received:", data
         try:    
             print 'writing to Arduino'
-            #arduino.write(data)    
+            arduino.write(data)    
             print 'wrote to Arduino'
-            time.sleep(.5)  
-            #print arduino.readline()  
+            #time.sleep(.5)  
+            lines_to_read = 0
+            while lines_to_read < 6:
+                print arduino.readline()
+                lines_to_read += 1
+                time.sleep(1) 
         except:    
             print "Failed to send!"   
--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -3,34 +3,50 @@
  * Degree-to-step translation is handled in Python by the client. TODO: handle flip at 360 degrees
  */
 
-#include <Stepper.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
 
-// number of steps on our motor
-#define STEPS 200
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
-// create an instance of the stepper class, specifying
-// the number of steps of the motor and the pins it's
-// attached to
-Stepper stepper(STEPS, 8, 9, 10, 11);
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
 
 int step_counter = 0;  // variable to count how many steps have been turned
 int target = 0;   // variable to hold target step count data from serial  
+String targetChar; // variable to hold target as string
+int inChar;
 
 void setup()
 {
-  stepper.setSpeed(30);  // set the speed of the motor to 30 RPMs
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotor->setSpeed(10);  // 10 rpm   
   Serial.begin(9600);    // open serial port
   Serial.print("Program Initiated\n");  
 }
+
 
 void loop()
 {
     // While data is sent over serial assign it to the msg  
     while (Serial.available()>0){   
-        target=Serial.read();  
-        Serial.print("Receiving Data\n"); 
-        Serial.print(target); 
-        Serial.print("\n"); 
+        // read new target
+        inChar = Serial.read();
+        if (isDigit(inChar)) {
+          targetChar += (char)inChar;
+        }
+        if (inChar == '\n') {
+          target = targetChar.toInt();
+          Serial.print("Receiving Data:\n"); 
+          Serial.println(target); 
+        }
     }  
 
   // step until step counter matches target
@@ -38,16 +54,25 @@ void loop()
       Serial.write(step_counter); // interpret 2000 as querying counter state
   } else if (target > step_counter) {
       while (target > step_counter){
-        stepper.step(1);  
-        step_counter++;
+        myMotor->step(1, FORWARD, SINGLE);  
+        ++step_counter;   
       }   
+      targetChar = "";
+      // print current status
+      Serial.println("Step Counter:");
+      Serial.println(step_counter); 
+      Serial.println("Target:");
+      Serial.println(target); 
   } else if (target < step_counter) {
       while (target < step_counter){
-        stepper.step(-1);  
-        step_counter--;
+        myMotor->step(1, BACKWARD, SINGLE);  
+        --step_counter;
       }   
-  }
-  Serial.print("Step Counter:\n");
-        Serial.print(step_counter); 
-        Serial.print("\n"); 
+      targetChar = "";
+      // print current status
+      Serial.println("Step Counter:");
+      Serial.println(step_counter); 
+      Serial.println("Target:");
+      Serial.println(target); 
+  } 
 }
